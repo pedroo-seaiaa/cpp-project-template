68780c5e825972a97e9fdcd563102be0e895483e
Pedro Oliveira <pedroo@squareenix-alchemy.com>
2023-12-11 13:46:12 +0900

    Refactors bash logger to its own file

--- configure.sh
+++ configure.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+LOGGER_PATH=$ROOT_DIR/tools/Utilities/logger.sh
 GIT_HOOKS_PATH=$ROOT_DIR/tools/GitTools/Hooks
 PYTHON_SETUP_SCRIPT_PATH=$ROOT_DIR/tools/BuildTools/setup.py
+
+source $LOGGER_PATH
 
 VERBOSE=0
 while getopts 'v' flag; do
     case "${flag}" in
     v)
         VERBOSE=1
+        LOG_VERBOSITY=1
+        set_log_level LOG_LEVEL_TRACE
         ;;
     ?)
         echo "script usage: $(basename \$0) [-v]" >&2
@@ -18,144 +23,8 @@ while getopts 'v' flag; do
 done
 
 # ############################################################################
-# TODO(PO): import logger.sh
-# ############################################################################
-
-# =============================================================================
-# Color Definitions
-# see also: https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
-# =============================================================================
-
-# Reset
-Color_Off='\033[0m' # Text Reset
-
-# Regular Colors
-Black='\033[0;30m'  # Black
-Red='\033[0;31m'    # Red
-Green='\033[0;32m'  # Green
-Yellow='\033[0;33m' # Yellow
-Blue='\033[0;34m'   # Blue
-Purple='\033[0;35m' # Purple
-Cyan='\033[0;36m'   # Cyan
-White='\033[0;37m'  # White
-
-# Bold
-BBlack='\033[1;30m'  # Black
-BRed='\033[1;31m'    # Red
-BGreen='\033[1;32m'  # Green
-BYellow='\033[1;33m' # Yellow
-BBlue='\033[1;34m'   # Blue
-BPurple='\033[1;35m' # Purple
-BCyan='\033[1;36m'   # Cyan
-BWhite='\033[1;37m'  # White
-
-# Underline
-UBlack='\033[4;30m'  # Black
-URed='\033[4;31m'    # Red
-UGreen='\033[4;32m'  # Green
-UYellow='\033[4;33m' # Yellow
-UBlue='\033[4;34m'   # Blue
-UPurple='\033[4;35m' # Purple
-UCyan='\033[4;36m'   # Cyan
-UWhite='\033[4;37m'  # White
-
-# Background
-On_Black='\033[40m'  # Black
-On_Red='\033[41m'    # Red
-On_Green='\033[42m'  # Green
-On_Yellow='\033[43m' # Yellow
-On_Blue='\033[44m'   # Blue
-On_Purple='\033[45m' # Purple
-On_Cyan='\033[46m'   # Cyan
-On_White='\033[47m'  # White
-
-# =============================================================================
-# Log Level Definitions
-# =============================================================================
-
-export CMAKE_LOG_LEVEL_TRACE=2
-export CMAKE_LOG_LEVEL_DEBUG=3
-export CMAKE_LOG_LEVEL_INFO=4
-export CMAKE_LOG_LEVEL_WARN=5
-export CMAKE_LOG_LEVEL_ERROR=6
-
-CMAKE_CURRENT_LOG_LEVEL=$CMAKE_LOG_LEVEL_INFO
-if [[ $VERBOSE = 1 ]]; then
-    CMAKE_CURRENT_LOG_LEVEL=$CMAKE_LOG_LEVEL_TRACE
-fi
-
-# =============================================================================
-# Functions Definitions
-# =============================================================================
-
-function log_message() {
-    echo -e "$1"
-}
-
-function log_trace() {
-    if [[ $CMAKE_CURRENT_LOG_LEVEL -le $CMAKE_LOG_LEVEL_TRACE ]]; then
-        if [[ $VERBOSE = 1 ]]; then
-            THIS_SCRIPT="[${0##*/}]"
-        else
-            THIS_SCRIPT=""
-        fi
-        log_message "$BWhite${THIS_SCRIPT}[T] $1 $Color_Off"
-    fi
-}
-
-function log_debug() {
-    if [[ $CMAKE_CURRENT_LOG_LEVEL -le $CMAKE_LOG_LEVEL_DEBUG ]]; then
-        if [[ $VERBOSE = 1 ]]; then
-            THIS_SCRIPT="[${0##*/}]"
-        else
-            THIS_SCRIPT=""
-        fi
-        log_message "$Cyan${THIS_SCRIPT}[D] $1 $Color_Off"
-    fi
-}
-
-function log_info() {
-    if [[ $VERBOSE = 1 ]]; then
-        THIS_SCRIPT="[${0##*/}]"
-    else
-        THIS_SCRIPT=""
-    fi
-    log_message "$Green${THIS_SCRIPT}[I] $1 $Color_Off"
-}
-
-function log_warn() {
-    if [[ $VERBOSE = 1 ]]; then
-        THIS_SCRIPT="[${0##*/}]"
-    else
-        THIS_SCRIPT=""
-    fi
-    log_message "$Yellow${THIS_SCRIPT}[W] $1 $Color_Off"
-}
-
-function log_error() {
-    if [[ $VERBOSE = 1 ]]; then
-        THIS_SCRIPT="[${0##*/}]"
-    else
-        THIS_SCRIPT=""
-    fi
-    log_message "$Red${THIS_SCRIPT}[E] $1 $Color_Off"
-}
-
-function log_fatal() {
-    if [[ $VERBOSE = 1 ]]; then
-        THIS_SCRIPT="[${0##*/}]"
-    else
-        THIS_SCRIPT=""
-    fi
-    log_message "$On_Red${THIS_SCRIPT}[F] $1$Color_Off"
-    exit 1
-}
-
-# ############################################################################
 # Functions
 # ############################################################################
-
-
 
 function setup_git_hooks() {
     log_trace "setup_git_hooks"
---
+++ tools/Utilities/logger.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+# =============================================================================
+# Color Definitions
+# see also: https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+# =============================================================================
+
+# Reset
+Color_Off='\033[0m' # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'  # Black
+Red='\033[0;31m'    # Red
+Green='\033[0;32m'  # Green
+Yellow='\033[0;33m' # Yellow
+Blue='\033[0;34m'   # Blue
+Purple='\033[0;35m' # Purple
+Cyan='\033[0;36m'   # Cyan
+White='\033[0;37m'  # White
+
+# Bold
+BBlack='\033[1;30m'  # Black
+BRed='\033[1;31m'    # Red
+BGreen='\033[1;32m'  # Green
+BYellow='\033[1;33m' # Yellow
+BBlue='\033[1;34m'   # Blue
+BPurple='\033[1;35m' # Purple
+BCyan='\033[1;36m'   # Cyan
+BWhite='\033[1;37m'  # White
+
+# Underline
+UBlack='\033[4;30m'  # Black
+URed='\033[4;31m'    # Red
+UGreen='\033[4;32m'  # Green
+UYellow='\033[4;33m' # Yellow
+UBlue='\033[4;34m'   # Blue
+UPurple='\033[4;35m' # Purple
+UCyan='\033[4;36m'   # Cyan
+UWhite='\033[4;37m'  # White
+
+# Background
+On_Black='\033[40m'  # Black
+On_Red='\033[41m'    # Red
+On_Green='\033[42m'  # Green
+On_Yellow='\033[43m' # Yellow
+On_Blue='\033[44m'   # Blue
+On_Purple='\033[45m' # Purple
+On_Cyan='\033[46m'   # Cyan
+On_White='\033[47m'  # White
+
+# =============================================================================
+# Log Level Definitions
+# =============================================================================
+
+LOG_LEVEL_TRACE=2
+LOG_LEVEL_DEBUG=3
+LOG_LEVEL_INFO=4
+LOG_LEVEL_WARN=5
+LOG_LEVEL_ERROR=6
+
+export LOG_VERBOSITY=0
+
+CURRENT_LOG_LEVEL=$LOG_LEVEL_INFO
+if [[ $LOG_VERBOSITY = 1 ]]; then
+    echo "changing logger verbosity"
+    CURRENT_LOG_LEVEL=$LOG_LEVEL_TRACE
+fi
+
+# =============================================================================
+# Functions Definitions
+# =============================================================================
+
+function set_log_level() {
+    CURRENT_LOG_LEVEL=$1
+}
+
+function log_message() {
+    echo -e "$1"
+}
+
+function log_trace() {
+    if [[ $CURRENT_LOG_LEVEL -le $LOG_LEVEL_TRACE ]]; then
+        if [[ $LOG_VERBOSITY = 1 ]]; then
+            THIS_SCRIPT="[${0##*/}]"
+        else
+            THIS_SCRIPT=""
+        fi
+        log_message "$BWhite${THIS_SCRIPT}[T] $1 $Color_Off"
+    fi
+}
+
+function log_debug() {
+    if [[ $CURRENT_LOG_LEVEL -le $LOG_LEVEL_DEBUG ]]; then
+        if [[ $LOG_VERBOSITY = 1 ]]; then
+            THIS_SCRIPT="[${0##*/}]"
+        else
+            THIS_SCRIPT=""
+        fi
+        log_message "$Cyan${THIS_SCRIPT}[D] $1 $Color_Off"
+    fi
+}
+
+function log_info() {
+    if [[ $LOG_VERBOSITY = 1 ]]; then
+        THIS_SCRIPT="[${0##*/}]"
+    else
+        THIS_SCRIPT=""
+    fi
+    log_message "$Green${THIS_SCRIPT}[I] $1 $Color_Off"
+}
+
+function log_warn() {
+    if [[ $LOG_VERBOSITY = 1 ]]; then
+        THIS_SCRIPT="[${0##*/}]"
+    else
+        THIS_SCRIPT=""
+    fi
+    log_message "$Yellow${THIS_SCRIPT}[W] $1 $Color_Off"
+}
+
+function log_error() {
+    if [[ $LOG_VERBOSITY = 1 ]]; then
+        THIS_SCRIPT="[${0##*/}]"
+    else
+        THIS_SCRIPT=""
+    fi
+    log_message "$Red${THIS_SCRIPT}[E] $1 $Color_Off"
+}
+
+function log_fatal() {
+    if [[ $LOG_VERBOSITY = 1 ]]; then
+        THIS_SCRIPT="[${0##*/}]"
+    else
+        THIS_SCRIPT=""
+    fi
+    log_message "$On_Red${THIS_SCRIPT}[F] $1$Color_Off"
+    exit 1
+}
