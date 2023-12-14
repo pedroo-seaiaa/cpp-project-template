4dd11fa5b927ec86fd040c136efb866c0fa85436
Pedro Oliveira <pedroo@squareenix-alchemy.com>
2023-12-14 10:10:27 +0900

    Need to work in WSL

--- tests/SetupTests/docker/linux.Dockerfile
+++ tests/SetupTests/docker/linux.Dockerfile
@@ -1,17 +1,8 @@
 # Use the official Ubuntu 22.04 LTS image as a parent image
 FROM ubuntu:22.04
 
-# Set the working directory in the container to /app
-WORKDIR /app
-
-# Copy the current directory contents into the container at /app
-COPY . /app
-
 # Update the system and install any packages you need
 RUN apt-get update
 
-# Make port 80 available to the world outside this container
-EXPOSE 80
-
 # Run your application when the container launches
-CMD ["echo", "hello-world"]
+CMD ["ls", "/antiphon"]
--- tests/SetupTests/scripts/testSetup.sh
+++ tests/SetupTests/scripts/testSetup.sh
@@ -1,144 +1,53 @@
 #!/bin/bash
 
-# =============================================================================
-# Color Definitions
-# see also: https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
-# =============================================================================
 
-# Reset
-Color_Off='\033[0m' # Text Reset
+# ============================================================================
+# Set PROJECT_ROOT_DIR
+# ============================================================================
 
-# Regular Colors
-Black='\033[0;30m'  # Black
-Red='\033[0;31m'    # Red
-Green='\033[0;32m'  # Green
-Yellow='\033[0;33m' # Yellow
-Blue='\033[0;34m'   # Blue
-Purple='\033[0;35m' # Purple
-Cyan='\033[0;36m'   # Cyan
-White='\033[0;37m'  # White
+# navigate to project root directory (this works as long as not in a submodule)
+git_dir=$(git rev-parse --show-toplevel)
 
-# Bold
-BBlack='\033[1;30m'  # Black
-BRed='\033[1;31m'    # Red
-BGreen='\033[1;32m'  # Green
-BYellow='\033[1;33m' # Yellow
-BBlue='\033[1;34m'   # Blue
-BPurple='\033[1;35m' # Purple
-BCyan='\033[1;36m'   # Cyan
-BWhite='\033[1;37m'  # White
+# import git utilities
+GIT_UTILITIES_SCRIPT_PATH=$git_dir/tools/GitTools//Utils/utilities.sh
+test -f $GIT_UTILITIES_SCRIPT_PATH || (echo "File not found: $GIT_UTILITIES_SCRIPT_PATH" && exit 1)
+source $GIT_UTILITIES_SCRIPT_PATH || (echo "Failed to source $GIT_UTILITIES_SCRIPT_PATH" && exit 1)
 
-# Underline
-UBlack='\033[4;30m'  # Black
-URed='\033[4;31m'    # Red
-UGreen='\033[4;32m'  # Green
-UYellow='\033[4;33m' # Yellow
-UBlue='\033[4;34m'   # Blue
-UPurple='\033[4;35m' # Purple
-UCyan='\033[4;36m'   # Cyan
-UWhite='\033[4;37m'  # White
+# detect if this tools folder is a submodule or not
+IS_SUBMODULE=$(
+    git_is_submodule
+    echo $?
+)
 
-# Background
-On_Black='\033[40m'  # Black
-On_Red='\033[41m'    # Red
-On_Green='\033[42m'  # Green
-On_Yellow='\033[43m' # Yellow
-On_Blue='\033[44m'   # Blue
-On_Purple='\033[45m' # Purple
-On_Cyan='\033[46m'   # Cyan
-On_White='\033[47m'  # White
-
-# =============================================================================
-# Log Level Definitions
-# =============================================================================
-
-LOG_LEVEL_TRACE=2
-LOG_LEVEL_DEBUG=3
-LOG_LEVEL_INFO=4
-LOG_LEVEL_WARN=5
-LOG_LEVEL_ERROR=6
-
-export LOG_VERBOSITY=1
-
-CURRENT_LOG_LEVEL=$LOG_LEVEL_INFO
-if [[ $LOG_VERBOSITY = 1 ]]; then
-    echo "changing logger verbosity"
-    CURRENT_LOG_LEVEL=$LOG_LEVEL_TRACE
+# getting the root level directory of the project
+PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
+if [[ $IS_SUBMODULE == 1 ]]; then
+    cd "${PROJECT_ROOT_DIR}/.."
+    PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
 fi
 
-# =============================================================================
-# Functions Definitions
-# =============================================================================
+# ============================================================================
+# Include logger
+# ============================================================================
 
-function set_log_level() {
-    CURRENT_LOG_LEVEL=$1
-}
+# import logger
+source ${PROJECT_ROOT_DIR}/tools/GitTools/Utils/logger.sh
+# set_log_level $LOG_LEVEL_TRACE
 
-function log_message() {
-    echo -e "$1"
-}
+# ============================================================================
+# Test Initial Project Setup through Docker
+# ============================================================================
 
-function log_trace() {
-    if [[ $CURRENT_LOG_LEVEL -le $LOG_LEVEL_TRACE ]]; then
-        if [[ $LOG_VERBOSITY = 1 ]]; then
-            THIS_SCRIPT="[${0##*/}]"
-        else
-            THIS_SCRIPT=""
-        fi
-        log_message "$BWhite${THIS_SCRIPT}[T] $1 $Color_Off"
-    fi
-}
+LINUX_DOCKERFILE_PATH=$PROJECT_ROOT_DIR/tests/SetupTests/docker/
+log_debug "LINUX_DOCKERFILE_PATH $LINUX_DOCKERFILE_PATH"
 
-function log_debug() {
-    if [[ $CURRENT_LOG_LEVEL -le $LOG_LEVEL_DEBUG ]]; then
-        if [[ $LOG_VERBOSITY = 1 ]]; then
-            THIS_SCRIPT="[${0##*/}]"
-        else
-            THIS_SCRIPT=""
-        fi
-        log_message "$Cyan${THIS_SCRIPT}[D] $1 $Color_Off"
-    fi
-}
+# ----------------------------------------------------------------------------
+# Declarations
+# ----------------------------------------------------------------------------
 
-function log_info() {
-    if [[ $LOG_VERBOSITY = 1 ]]; then
-        THIS_SCRIPT="[${0##*/}]"
-    else
-        THIS_SCRIPT=""
-    fi
-    log_message "$Green${THIS_SCRIPT}[I] $1 $Color_Off"
-}
+log_info " === Running Initial Project Setup (through Docker) === "
 
-function log_warn() {
-    if [[ $LOG_VERBOSITY = 1 ]]; then
-        THIS_SCRIPT="[${0##*/}]"
-    else
-        THIS_SCRIPT=""
-    fi
-    log_message "$Yellow${THIS_SCRIPT}[W] $1 $Color_Off"
-}
-
-function log_error() {
-    if [[ $LOG_VERBOSITY = 1 ]]; then
-        THIS_SCRIPT="[${0##*/}]"
-    else
-        THIS_SCRIPT=""
-    fi
-    log_message "$Red${THIS_SCRIPT}[E] $1 $Color_Off"
-}
-
-function log_fatal() {
-    if [[ $LOG_VERBOSITY = 1 ]]; then
-        THIS_SCRIPT="[${0##*/}]"
-    else
-        THIS_SCRIPT=""
-    fi
-    log_message "$On_Red${THIS_SCRIPT}[F] $1$Color_Off"
-    exit 1
-}
-
-
-log_info " === Running Initial Configuration Setup === "
+log_warn "TODO: Detect docker"
 
 log_warn "Detecting docker image tagged with aiaa-linux ..."
 IMAGE_ID=$(docker images -q aiaa-linux)
@@ -146,7 +55,7 @@ if [ "$IMAGE_ID" = "" ]; then
     log_error "Detecting docker image tagged with aiaa-linux --- FAILURE"
     
     log_warn "Building aiaa-linux from Dockerfile ..."
-    docker build -f ../docker/linux.Dockerfile ../docker/ --tag aiaa-linux > /dev/null
+    docker build -f ${LINUX_DOCKERFILE_PATH}/linux.Dockerfile ${LINUX_DOCKERFILE_PATH} --tag aiaa-linux
     IMAGE_ID=$(docker images -q aiaa-linux)
     if [ "$IMAGE_ID" = "" ]; then
         log_fatal "Building aiaa-linux from Dockerfile --- FAILURE"
@@ -157,9 +66,10 @@ else
     log_info "Detecting docker image tagged with aiaa-linux --- SUCCESS"
 fi
 
-log_warn "Running project setup through aiaa-linux ..."
-docker run --rm aiaa-linux
+exit 0
 
+log_warn "Running project setup through aiaa-linux ..."
+docker run --rm aiaa-linux -v ${PROJECT_ROOT_DIR}:/antiphon # removes container but not the image, which is okay
 
 # # debug
 log_debug ""
